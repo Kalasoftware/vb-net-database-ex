717e00fb02596296b689739d2261f10e6cdeec10
Sumeet Patel <Sumeet Patel@DESKTOP-HO2PUH5>
2024-03-05 17:31:50 +0530

    Add project files.

---
+++ inventory/Form1.Designer.vb
@@ -0,0 +1,163 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.NumericUpDown1 = New System.Windows.Forms.NumericUpDown()
+        Me.txt_price = New System.Windows.Forms.TextBox()
+        Me.txt_company = New System.Windows.Forms.TextBox()
+        Me.DateTimePicker1 = New System.Windows.Forms.DateTimePicker()
+        Me.DateTimePicker2 = New System.Windows.Forms.DateTimePicker()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.cmpbox = New System.Windows.Forms.ComboBox()
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"CHOCOLATE", "BISCUIT", "OREO", "BREAD", "PEANUTS", "BUTTER", "CHEESE", "WHEAT BREAD"})
+        Me.ComboBox1.Location = New System.Drawing.Point(98, 47)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(121, 21)
+        Me.ComboBox1.TabIndex = 0
+        '
+        'NumericUpDown1
+        '
+        Me.NumericUpDown1.Location = New System.Drawing.Point(108, 95)
+        Me.NumericUpDown1.Name = "NumericUpDown1"
+        Me.NumericUpDown1.Size = New System.Drawing.Size(120, 20)
+        Me.NumericUpDown1.TabIndex = 1
+        '
+        'txt_price
+        '
+        Me.txt_price.Location = New System.Drawing.Point(119, 147)
+        Me.txt_price.Name = "txt_price"
+        Me.txt_price.Size = New System.Drawing.Size(100, 20)
+        Me.txt_price.TabIndex = 2
+        '
+        'txt_company
+        '
+        Me.txt_company.Location = New System.Drawing.Point(415, 121)
+        Me.txt_company.Name = "txt_company"
+        Me.txt_company.Size = New System.Drawing.Size(100, 20)
+        Me.txt_company.TabIndex = 3
+        '
+        'DateTimePicker1
+        '
+        Me.DateTimePicker1.Location = New System.Drawing.Point(315, 94)
+        Me.DateTimePicker1.Name = "DateTimePicker1"
+        Me.DateTimePicker1.Size = New System.Drawing.Size(200, 20)
+        Me.DateTimePicker1.TabIndex = 4
+        '
+        'DateTimePicker2
+        '
+        Me.DateTimePicker2.Location = New System.Drawing.Point(315, 147)
+        Me.DateTimePicker2.Name = "DateTimePicker2"
+        Me.DateTimePicker2.Size = New System.Drawing.Size(200, 20)
+        Me.DateTimePicker2.TabIndex = 5
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(27, 13)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 6
+        Me.Button1.Text = "insert"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(471, 12)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 7
+        Me.Button2.Text = "update"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(119, 188)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(394, 150)
+        Me.DataGridView1.TabIndex = 8
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(141, 22)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(39, 13)
+        Me.Label8.TabIndex = 9
+        Me.Label8.Text = "Label1"
+        '
+        'cmpbox
+        '
+        Me.cmpbox.FormattingEnabled = True
+        Me.cmpbox.Location = New System.Drawing.Point(353, 47)
+        Me.cmpbox.Name = "cmpbox"
+        Me.cmpbox.Size = New System.Drawing.Size(121, 21)
+        Me.cmpbox.TabIndex = 10
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.cmpbox)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.DateTimePicker2)
+        Me.Controls.Add(Me.DateTimePicker1)
+        Me.Controls.Add(Me.txt_company)
+        Me.Controls.Add(Me.txt_price)
+        Me.Controls.Add(Me.NumericUpDown1)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents NumericUpDown1 As NumericUpDown
+    Friend WithEvents txt_price As TextBox
+    Friend WithEvents txt_company As TextBox
+    Friend WithEvents DateTimePicker1 As DateTimePicker
+    Friend WithEvents DateTimePicker2 As DateTimePicker
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents Label8 As Label
+    Friend WithEvents cmpbox As ComboBox
+End Class
---
+++ inventory/Form1.vb
@@ -0,0 +1,125 @@
+﻿Imports System.Data.OleDb
+Imports System.Reflection.Emit
+Public Class Form1
+    Dim connection As OleDbConnection
+    Dim ds As DataSet
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim connection_string As New String("Data Source=localhost;" +
+                                             "password=it4; User id=it4; " +
+                                             "Provider=ORAOLEDB.Oracle")
+        connection = New OleDbConnection(connection_string)
+
+        fillDataGrid()
+        fillcompanyc()
+    End Sub
+
+    Private Sub fillcompanyc()
+        Dim dataAdapter As New OleDbDataAdapter("Select * from COMPANY_C", connection)
+        dataAdapter.Fill(ds, "company_C")
+        cmpbox.DataSource = ds.Tables("company_c")
+        cmpbox.DisplayMember = "CNAME"
+        cmpbox.ValueMember = "CID"
+
+
+    End Sub
+
+    Private Sub fillDataGrid()
+        ds = New DataSet
+        Dim dataAdapter As New OleDbDataAdapter _
+            ("Select * from INVENTORY_C", connection)
+        dataAdapter.Fill(ds)
+        DataGridView1.DataSource = ds.Tables(0)
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Try
+            connection.Open()
+            Dim qry As String
+            qry = "INSERT INTO INVENTORY_C " &
+ "( ITEM_NAME, QUANTITY, TOTAL_PRICE, PAYMENT_DATE, DELIVERY_DATE,COMPANY_ID) 
+VALUES ( ?, ?, ?, TO_DATE(?, 'YYYY-MM-DD'), TO_DATE(?, 'YYYY-MM-DD')),?"
+
+            Dim command As New OleDbCommand(qry, connection)
+            command.Parameters.AddWithValue("?", ComboBox1.SelectedItem.ToString)
+            command.Parameters.AddWithValue("?", NumericUpDown1.Value)
+            command.Parameters.AddWithValue("?", CInt(txt_price.Text))
+            ' command.Parameters.AddWithValue("?", txt_company.Text)
+            command.Parameters.AddWithValue("?", DateTimePicker1.Value.ToString("yyyy-MM-dd"))
+            command.Parameters.AddWithValue("?", DateTimePicker2.Value.ToString("yyyy-MM-dd"))
+            command.Parameters.AddWithValue("?", cmpbox.SelectedValue)
+
+
+            Dim insrow = command.ExecuteNonQuery()
+            connection.Close()
+            If (insrow >= 1) Then
+                MsgBox("Data Inserted")
+                fillDataGrid()
+            End If
+        Catch ex As Exception
+            MsgBox(ex.ToString)
+        End Try
+    End Sub
+
+    Private Sub DataGridView1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellContentClick
+
+    End Sub
+
+    Private Sub DataGridView1_SelectionChanged(sender As Object, e As EventArgs) Handles DataGridView1.SelectionChanged
+        Try
+
+            Label8.Text = DataGridView1.SelectedRows(0).Cells(0).Value
+            ComboBox1.SelectedItem = DataGridView1.SelectedRows(0).Cells(1).Value
+            NumericUpDown1.Value = DataGridView1.SelectedRows(0).Cells(2).Value
+            txt_price.Text = DataGridView1.SelectedRows(0).Cells(3).Value
+            txt_company.Text = DataGridView1.SelectedRows(0).Cells(4).Value
+            DateTimePicker1.Value = DataGridView1.SelectedRows(0).Cells(5).Value
+            DateTimePicker2.Value = DataGridView1.SelectedRows(0).Cells(6).Value
+        Catch ex As Exception
+
+        End Try
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Try
+
+            connection.Open()
+            Dim updateQry = "Update INVENTORY_C SET ITEM_NAME=?, QUANTITY=?, TOTAL_PRICE =?, PAYMENT_DATE=?, DELIVERY_DATE =?, COMPANY_ID=? 
+WHERE IID=?"
+            Dim cmd As New OleDbCommand(updateQry, connection)
+
+            cmd.Parameters.AddWithValue("?", ComboBox1.SelectedItem)
+            cmd.Parameters.AddWithValue("?", NumericUpDown1.Value)
+            cmd.Parameters.AddWithValue("?", CInt(txt_price.Text))
+
+            cmd.Parameters.AddWithValue("?", DateTimePicker1.Value.ToString("yyyy-MM-dd"))
+            cmd.Parameters.AddWithValue("?", DateTimePicker2.Value.ToString("yyyy-MM-dd"))
+            cmd.Parameters.AddWithValue("?", cmpbox.SelectedValue)
+            cmd.Parameters.AddWithValue("?", Label8.Text)
+
+            Dim res = cmd.ExecuteNonQuery()
+            connection.Close()
+
+            If (res >= 1) Then
+                MsgBox("Data updated")
+                fillDataGrid()
+            End If
+
+
+        Catch ex As Exception
+
+        End Try
+
+
+    End Sub
+
+    Private Sub cmpbox_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cmpbox.SelectedIndexChanged
+
+
+
+    End Sub
+End Class
+
+
